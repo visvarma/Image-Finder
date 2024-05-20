9b00f44f7d90e1d0edd846113acd3453da92c6e1
visvarma <visvarma2021@gmail.com>
2024-05-20 21:47:24 +0530

    Implement multi step form with demo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import MuliStepForm from "./components/MuliStepForm";
 
-function App() {
-  return <h1 className="text-2xl font bold text-red-400">Image Finder</h1>;
+export default function App() {
+  const componentList = [<Example1 />, <Example2 />, <Example3 />];
+
+  return (
+    <div className="flex justify-center flex-col items-center p-6 bg-zinc-700 gap-4">
+      <h1 className="text-3xl font-bold text-lime-400">Image Finder</h1>
+      <MuliStepForm componentList={componentList} />
+    </div>
+  );
 }
 
-export default App;
+const Example1 = () => {
+  return <h1 className="text-white">step 1</h1>;
+};
+const Example2 = () => {
+  return <h1 className="text-white">step 2</h1>;
+};
+const Example3 = () => {
+  return <h1 className="text-white">step 3</h1>;
+};
--- src/components/MuliStepForm.jsx
+++ src/components/MuliStepForm.jsx
@@ -1,5 +1,47 @@
-const MuliStepForm = () => {
-  return <div>MuliStepForm</div>;
+import React, { useMemo, useState } from "react";
+
+const MuliStepForm = ({ componentList }) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const stepsCount = componentList.length;
+
+  const handleNextStep = () => {
+    if (currentStep + 1 < componentList.length) {
+      setCurrentStep((prevStep) => prevStep + 1);
+    }
+  };
+
+  const handleCurrentStep = (index) => {
+    setCurrentStep(index);
+  };
+
+  const progressLength = useMemo(() => {
+    return (100 / (stepsCount - 1)) * currentStep;
+  }, [currentStep]);
+
+  return (
+    <div className="w-full max-w-screen-md border-2 border-blue-300 p-5">
+      <div className="step-inidcator mb-10 flex justify-between w-full border-3 border-cyan-50 relative">
+        {componentList?.map((_, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => handleCurrentStep(index)}
+              className="w-5 h-5 rounded-full bg-lime-400 p-5 flex justify-center items-center z-10 font-bold"
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+        <div
+          className="step-progress absolute top-[50%] h-1 bg-lime-600 "
+          style={{ width: `${progressLength}%` }}
+        ></div>
+      </div>
+      <div className="active-component flex justify-center">
+        {componentList[currentStep]}
+      </div>
+    </div>
+  );
 };
 
 export default MuliStepForm;
